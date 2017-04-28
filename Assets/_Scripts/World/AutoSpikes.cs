038c18aea790729c080094cc0381911631674a35
teenik <teenik3315@gmail.com>
2017-04-28 23:37:01 +0300

    autospikes

---
+++ Assets/_Scripts/World/AutoSpikes.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoSpikes : MonoBehaviour {
+
+    public float interval = 1.5f;
+    private float time = 0f;
+    private bool enab = false;
+
+	void Update () {
+        time += Time.deltaTime;
+
+        if(time > interval)
+        {
+            time = 0;
+            enab = !enab;
+            transform.GetChild(0).gameObject.SetActive(enab);       
+        }
+	}
+}
